b1eb0e8ca0f1b252662feb3a2489f677a4f5c0c0
Javier <193231@ids.upchiapas.edu.mx>
2020-11-11 23:49:22 -0600

    Register component business logic and service register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { RegisterService } from '../service/register/register.service'
 
 @Component({
   selector: 'app-register',
@@ -8,20 +9,50 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
 
-  registerFormGroup:FormGroup;
-  hide:boolean=true;
+  registerFormGroup: FormGroup;
+  hidePassword1: boolean = true;
+  hidePassword2: boolean = true;
 
-  constructor(private _formBuilder: FormBuilder) { }
+  constructor(private _formBuilder: FormBuilder, private _RegisterRegister: RegisterService) { }
 
   ngOnInit(): void {
-    this.registerFormGroup=this._formBuilder.group({
-      username: ['',Validators.required],
-      email: ['', [Validators.email, Validators.required]],
-      password: ['', Validators.required]
-    })
+    this.setForm();
   }
 
-  signup(){
-    
+  setForm(){
+    this.registerFormGroup = this._formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.email, Validators.required]],
+      password1: ['', [Validators.required, Validators.minLength(8)]],
+      password2: ['', Validators.required],
+    }, {
+      validators: this.validarPassword
+    });
+  }
+
+  validarPassword: ValidatorFn = (
+    control: FormGroup
+  ): ValidationErrors | null => {
+    const password = control.get('password1')
+    const confirmarPassword = control.get('password2')
+  
+    return password.value === confirmarPassword.value
+      ? null
+      : { noSonIguales: true }
+  }
+
+  checkPassword(): boolean {
+    return this.registerFormGroup.hasError('noSonIguales') &&
+      this.registerFormGroup.get('password1').dirty &&
+      this.registerFormGroup.get('password2').dirty;
+  }
+
+  signup() {
+    const data = this.registerFormGroup.value
+    this._RegisterRegister.signup(data.username,data.email,data.password1,data.password2).subscribe(Response=>{
+      this.registerFormGroup.reset();
+    }, error => {
+      console.log(error.error)
+    })
   }
 }
---
+++ src/app/service/register/register.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+
+  api: String = 'https://back-web-193231-ids.herokuapp.com/';
+  constructor(private http: HttpClient) { }
+
+  signup(username: String, email: String, password1: String, password2: String): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    }
+    return this.http.post(`${this.api}api/v1/sign_up/signup`, { username, email, password1, password2 }, httpOptions);
+  }
+}
